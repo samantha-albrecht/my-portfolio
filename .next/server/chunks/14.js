bdc6af57f68a344f2a80044f503c2145e5e6019e
Samantha <56079800+samantha-albrecht@users.noreply.github.com>
2022-01-20 21:45:45 -0600

    run next.js build

---
+++ .next/server/chunks/14.js
@@ -0,0 +1,79 @@
+exports.id = 14;
+exports.ids = [14];
+exports.modules = {
+
+/***/ 6014:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _SectionContainer_module_scss__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(6082);
+/* harmony import */ var _SectionContainer_module_scss__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_SectionContainer_module_scss__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var classnames__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(4058);
+/* harmony import */ var classnames__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(classnames__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(5282);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__);
+
+
+
+
+
+function SectionContainer({
+  children,
+  section
+}) {
+  return /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx("div", {
+    className: classnames__WEBPACK_IMPORTED_MODULE_0___default()((_SectionContainer_module_scss__WEBPACK_IMPORTED_MODULE_2___default().container), {
+      [(_SectionContainer_module_scss__WEBPACK_IMPORTED_MODULE_2___default())[`section${section}`]]: section
+    }),
+    children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)("div", {
+      className: (_SectionContainer_module_scss__WEBPACK_IMPORTED_MODULE_2___default().gridLayout),
+      children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx("div", {
+        className: (_SectionContainer_module_scss__WEBPACK_IMPORTED_MODULE_2___default().backgroundContainer)
+      }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)("div", {
+        className: (_SectionContainer_module_scss__WEBPACK_IMPORTED_MODULE_2___default().guidesContainer),
+        children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx("div", {
+          className: (_SectionContainer_module_scss__WEBPACK_IMPORTED_MODULE_2___default().guide)
+        }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx("div", {
+          className: (_SectionContainer_module_scss__WEBPACK_IMPORTED_MODULE_2___default().guide)
+        }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx("div", {
+          className: (_SectionContainer_module_scss__WEBPACK_IMPORTED_MODULE_2___default().guide)
+        }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx("div", {
+          className: (_SectionContainer_module_scss__WEBPACK_IMPORTED_MODULE_2___default().guide)
+        })]
+      }), children]
+    })
+  });
+}
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (SectionContainer);
+
+/***/ }),
+
+/***/ 6082:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"container": "SectionContainer_container__1JPgw",
+	"gridLayout": "SectionContainer_gridLayout__3TYry",
+	"backgroundContainer": "SectionContainer_backgroundContainer__2cYAs",
+	"guidesContainer": "SectionContainer_guidesContainer__3o72v",
+	"guide": "SectionContainer_guide__2z15k",
+	"sectionHeader": "SectionContainer_sectionHeader__Ao1CR",
+	"sectionHero": "SectionContainer_sectionHero__1gAxh",
+	"sectionWork": "SectionContainer_sectionWork__2XNY6",
+	"sectionWorkTitle": "SectionContainer_sectionWorkTitle__2iYRo",
+	"sectionProcess": "SectionContainer_sectionProcess__2Jz_S",
+	"sectionBooks": "SectionContainer_sectionBooks__GFF_b",
+	"sectionAbout": "SectionContainer_sectionAbout__19BUU",
+	"sectionFooter": "SectionContainer_sectionFooter__22uKi"
+};
+
+
+/***/ })
+
+};
+;
---
+++ .next/server/chunks/273.js
@@ -0,0 +1,35 @@
+exports.id = 273;
+exports.ids = [273];
+exports.modules = {
+
+/***/ 9273:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"contentContainer": "ProjectContainer_contentContainer__n4VIH",
+	"projectAsideContainer": "ProjectContainer_projectAsideContainer__vio3U",
+	"projectHeadingContainer": "ProjectContainer_projectHeadingContainer__vkJ00",
+	"projectBodyContainer": "ProjectContainer_projectBodyContainer__1UP9F",
+	"projectTocItem": "ProjectContainer_projectTocItem__2eIYc",
+	"projectDetailsContainer": "ProjectContainer_projectDetailsContainer__O6mCm",
+	"projectCaption": "ProjectContainer_projectCaption__19UWp",
+	"projectTitle": "ProjectContainer_projectTitle__3bEkM",
+	"imageContainer": "ProjectContainer_imageContainer__2vPM2",
+	"imageMain": "ProjectContainer_imageMain__37uZb",
+	"projectBodyTitle": "ProjectContainer_projectBodyTitle__1VQ8b",
+	"projectBodySubTitleLg": "ProjectContainer_projectBodySubTitleLg__1NZU_",
+	"projectBodySubTitleMd": "ProjectContainer_projectBodySubTitleMd__13dag",
+	"projectBodyParagraph": "ProjectContainer_projectBodyParagraph__3A-4G",
+	"projectBodyList": "ProjectContainer_projectBodyList__1q_dG",
+	"projectDetailsGroup": "ProjectContainer_projectDetailsGroup__1Q3HI",
+	"projectDetailsTitle": "ProjectContainer_projectDetailsTitle__3Sp6T",
+	"projectDetailsListItem": "ProjectContainer_projectDetailsListItem__2U8_c",
+	"imageSmall": "ProjectContainer_imageSmall__2Zj_J"
+};
+
+
+/***/ })
+
+};
+;
